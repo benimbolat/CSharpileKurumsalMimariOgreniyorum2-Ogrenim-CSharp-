ac325eb2c84ec4f1153d387f0d2407a9b60f26a7
Muhammed Enes Bolat <benimbolat@outlook.com>
2021-03-25 18:43:32 +0300

    2021.03.25-18:44

---
+++ DevFramework.Northwind.Business.Tests/EntityFrameworkTests/EntityFrameworkTest.cs
@@ -0,0 +1,21 @@
+﻿using DevFramework.Northwind.DataAccess.Concrete.EntityFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using DevFramework.Northwind.Entities;
+
+namespace DevFramework.Northwind.Business.Tests.EntityFrameworkTests
+{
+    [TestClass]
+    public class EntityFrameworkTest
+    {
+        [TestMethod]
+        public void Get_all_returns_all_products()
+        {
+            EfProductDal productDal = new EfProductDal();
+
+            var result = productDal.GetList();
+
+            Assert.AreEqual(77, result.Count);
+        }
+    }
+}
