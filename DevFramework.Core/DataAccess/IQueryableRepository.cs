9a2e19390cbf85b82d95a7a1da51424c0b902fe7
Muhammed Enes Bolat <benimbolat@outlook.com>
2021-03-25 14:56:34 +0300

    2021.03.25-2:56

--- DevFramework.Core/DataAccess/IQueryableRepository.cs
+++ DevFramework.Core/DataAccess/IQueryableRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevFramework.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace DevFramework.Core.DataAccess
 {
-    interface IQueryableRepository
-    {
+    public interface IQueryableRepository<T> where T : class, IEntity/*İmplemente edilme şartı*/, new()
+    { 
+        IQueryable<T> Table { get; }
     }
 }
---
+++ DevFramework.Core/DataAccess/NHihabernate/NHibernateHelper.cs
@@ -0,0 +1,30 @@
+﻿using NHibernate; //hibernate 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAccess.NHihabernate
+{
+    public abstract class NHibernateHelper : IDisposable
+    {
+        private static ISessionFactory _sessionFactory;
+        public virtual ISessionFactory SessionFactory
+        {
+            get
+            {
+                return _sessionFactory ?? (_sessionFactory = Initializefactory());
+            }
+        }
+        protected abstract ISessionFactory Initializefactory();
+        public virtual ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+        public void Dispose()
+        {
+           GC.SuppressFinalize(this.);
+        }
+    }
+}
